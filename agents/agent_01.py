d156cf5b36c73566dce0a4a87495f71008fa016a
rajesh-nitc <rajesh.nitc@gmail.com>
2025-01-19 04:02:57 +0000

    refactor: agent naming

--- agents/agent_01.py
+++ agents/agent_01.py
@@ -1,5 +1,3 @@
-import logging
-
 from semantic_kernel.agents import ChatCompletionAgent
 from semantic_kernel.connectors.ai.function_choice_behavior import (
     FunctionChoiceBehavior,
@@ -10,20 +8,18 @@ from semantic_kernel.connectors.ai.google.vertex_ai import (
 )
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.contents.chat_history import ChatHistory
-from semantic_kernel.contents.utils.author_role import AuthorRole
 from semantic_kernel.kernel import Kernel
 
 from config.settings import settings
 from plugins.add_plugins import add_plugins
 
-logger = logging.getLogger(__name__)
 
-
-async def get_agent():
+async def get_agent_01():
     kernel = Kernel()
     add_plugins(kernel)
 
     service_id = "agent"
+    agent_name = "agent_01"
 
     if "gpt" in settings.LLM_MODEL:
         kernel.add_service(
@@ -42,7 +38,7 @@ async def get_agent():
         return ChatCompletionAgent(
             service_id=service_id,
             kernel=kernel,
-            name=settings.APP_NAME,
+            name=agent_name,
             instructions=settings.LLM_SYSTEM_INSTRUCTION,
             execution_settings=execution_settings,
         )
@@ -60,7 +56,7 @@ async def get_agent():
         execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
         return ChatCompletionAgent(
             kernel=kernel,
-            name=settings.APP_NAME,
+            name=agent_name,
             instructions=settings.LLM_SYSTEM_INSTRUCTION,
             execution_settings=execution_settings,
         )
@@ -68,31 +64,27 @@ async def get_agent():
         raise ValueError(f"Unsupported model type: {settings.LLM_MODEL}")
 
 
-async def invoke_agent(
+async def invoke_agent_01(
     agent: ChatCompletionAgent, prompt: str, chat: ChatHistory
 ) -> str:
     """
     Invoke the agent with the user prompt.
     """
-    agent = await get_agent()
+    agent = await get_agent_01()
     chat.add_user_message(prompt)
-    print(f"# {AuthorRole.USER}: '{prompt}'")
 
     streaming = False
     response_content = ""
 
     if streaming:
         contents = []
-        content_name = ""
         async for content in agent.invoke_stream(chat):
-            content_name = content.name
+            content.name
             contents.append(content)
         response_content = "".join([content.content for content in contents])
-        print(f"# {content.role} - {content_name or '*'}: '{response_content}'")
         chat.add_assistant_message(response_content)
     else:
         async for content in agent.invoke(chat):
             response_content = content.content
-            print(f"# {content.role} - {content.name or '*'}: '{content.content}'")
         chat.add_message(content)
     return response_content
--- routers/prompt.py
+++ routers/prompt.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from semantic_kernel.agents import ChatCompletionAgent
 
-from agents.func_calling import get_agent
+from agents.agent_01 import get_agent_01
 from models.prompt import PromptRequest, PromptResponse
 from services.llm import generate_model_response
 
@@ -11,7 +11,7 @@ router: APIRouter = APIRouter()
 @router.post("/prompt", response_model=PromptResponse)
 async def get_prompt_response(
     request: PromptRequest,
-    agent: ChatCompletionAgent = Depends(get_agent),
+    agent: ChatCompletionAgent = Depends(get_agent_01),
 ) -> PromptResponse:
     result = await generate_model_response(agent, request.prompt, request.user_id)
     return PromptResponse(response=result)
