19e172b9194cdc589b98f6bc4054c6d4eda709f1
rajesh-nitc <rajesh.nitc@gmail.com>
2025-03-04 05:17:45 +0000

    chore: run ci

--- plugins/add_plugins.py
+++ plugins/add_plugins.py
@@ -3,6 +3,7 @@ from semantic_kernel.kernel import Kernel
 from plugins.weather import WeatherPlugin
 
 
+# TODO: add plugins as part of agent initialization
 def add_plugins(kernel: Kernel):
     kernel.add_plugin(WeatherPlugin(), plugin_name="weather")
     # Add more plugins here as needed
